8cbd6bcc3bb4308279b8576ddea52af66f047528
ELANGO777 <98794891+ELANGO777@users.noreply.github.com>
2022-05-02 21:11:16 +0530

    Update Baseclass.java
    e

--- elango/src/test/java/Base/Baseclass.java
+++ elango/src/test/java/Base/Baseclass.java
@@ -15,6 +15,7 @@ public class Baseclass {
 	    public Baseclass(WebDriver driver) throws InterruptedException {
 	        this.driver = driver;
 	        wait =new WebDriverWait(driver, Duration.ofSeconds(5));
+		    int elango1;
 	        int elango;
 	    }
 
