842f35c1dfc9c1e3423397522fcb9fa9245639a7
elangovan <elangovan2435@gmail.com>
2022-05-03 22:45:05 +0530

    t2

--- elango/src/test/java/Base/Baseclass.java
+++ elango/src/test/java/Base/Baseclass.java
@@ -15,7 +15,7 @@ public class Baseclass {
 	    public Baseclass(WebDriver driver) throws InterruptedException {
 	        this.driver = driver;
 	        wait =new WebDriverWait(driver, Duration.ofSeconds(5));
-		  
+	        return wait.until(ExpectedConditions.visibilityOf(element));
 	        int elango222222;
 	    }
 
